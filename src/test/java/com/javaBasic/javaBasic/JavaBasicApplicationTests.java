935f6681b1a2529494dff1ff3b4a1ebb4bda0137
xivic434 <xivic@kakao.com>
2022-02-13 21:08:39 +0900

    start beginner education

---
+++ src/test/java/com/javaBasic/javaBasic/JavaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaBasic.javaBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
